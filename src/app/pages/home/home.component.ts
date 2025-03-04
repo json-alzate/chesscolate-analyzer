106f565d7c668f0afb9866175ee7cc35446a8137
Json Alzate <developer.alzate@gmail.com>
2025-03-04 10:11:53 -0500

    columnPawnAdvanced init

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,26 +10,39 @@ import { ReadPgnService } from '../../services/read-pgn.service';
 })
 export default class HomeComponent {
 
-  constructor(private readPgnService: ReadPgnService) {}
+  constructor(private readPgnService: ReadPgnService) { }
 
   async readFile() {
 
     try {
       const pgnText = await this.readPgnService.loadPgnFile();
 
-
       const partidas: any[] = [];
       const juegos = pgnText.split(/\n\n(?=\[Event)/); // Divide las partidas en bloques
-    
+
       juegos.forEach((juego) => {
         partidas.push(juego);
       });
-    
-      // console.log(partidas);
+
+      console.log(partidas[0]);
 
     } catch (error) {
       console.error('Error al cargar el PGN:', error);
     }
   }
+
+  /*
+    Se calcula en que columna se avanza mas un peon
+  */
+  columnPawnAdvanced(games: any[]) {
+
+    for (const game of games) {
+
+    }
+
+  }
+
+
+
 
 }
